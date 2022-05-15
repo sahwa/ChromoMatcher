8b24d577eb66252beec65176c830edda9aa1c837
sahwa <ucbtsm8@ucl.ac.uk>
2022-05-15 14:06:24 +0100

    5 May

---
+++ code/make_PC_plots.R
@@ -0,0 +1,50 @@
+R
+
+setwd("/home/sam/Dropbox/Documents/PhD/PhD/analyses/ChromoMatcher/finestructure/data")
+
+chunkcounts = sort(list.files(pattern="chunkcounts.out"))
+
+make_pca = function(x, n_pcs = 10) {
+	x_dat = fread(x, header=T)
+	x_dat = melt(x_dat, id.vars="Recipient"
+			)[, value := fifelse(Recipient == variable, mean(value), value), Recipient
+				][, dcast(.SD, Recipient~variable)]
+
+	pca_res = irlba::prcomp_irlba(x_dat[,-1], n=10)
+	pca = data.table(ind = x_dat$Recipient, irlba::prcomp_irlba(x_dat[,-1], n=10)$x)
+	pca$pop = str_remove_all(pca$ind, "[0-9]")
+
+	n_pcs_vec = 1:n_pcs
+	combinations = Map(c, n_pcs_vec[-length(n_pcs_vec)], n_pcs_vec[-1])
+
+	plot_res_list = vector(length = length(n_pcs_vec), mode='list')
+
+	plot_res_list = lapply(combinations, function(x) {
+		pca %>%
+			ggplot(aes(x=get(paste0("PC",x[[1]])), y=get(paste0("PC",x[[2]])))) +
+			geom_point(alpha=0.6, aes(colour=pop)) +
+			theme_light() + 
+			theme(legend.position="none") +
+			xlab(paste0("PC", x[[1]])) +
+			ylab(paste0("PC", x[[2]])) +
+			theme(
+				axis.text = element_text(size=16),
+				axis.title = element_text(size=20)
+				)
+	})
+
+	n = length(plot_res_list)
+	nCol = floor(sqrt(n))
+	a = do.call(gridExtra::grid.arrange, c(plot_res_list, ncol=nCol))
+	out = paste0(str_remove_all(x, ".chunkcounts.out"), ".pdf")
+	ggsave(filename=out, a, width=25.9, height=25.9, units="in")
+	return(0)
+}
+
+sapply(chunkcounts, make_pca)
+
+
+return_PCS = function(x) {
+	x = fread(x, header=T)
+	pca = data.table(ind = x_dat$Recipient, irlba::prcomp_irlba(x_dat[,-1], n=10)$x)
+}
